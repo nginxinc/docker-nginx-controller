fd5998f8c050e31234ffd80c207d686aa3768e77
Artur Zych <a.zych@f5.com>
2020-11-03 13:53:54 +0100

    prefix location and store_uuid

--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -43,8 +43,8 @@ test -n "${ENV_CONTROLLER_INSTANCE_NAME}" && \
 test -n "${ENV_CONTROLLER_URL}" && \
     controller_url=${ENV_CONTROLLER_URL}
 
-test -n "${ENV_LOCATION}" && \
-    location=${ENV_LOCATION}
+test -n "${ENV_CONTROLLER_LOCATION}" && \
+    location=${ENV_CONTROLLER_LOCATION}
 
 if [ -n "${api_key}" -o -n "${instance_name}" -o -n "${controller_url}" -o -n "${location}" ]; then
     echo "updating ${agent_conf_file} ..."
